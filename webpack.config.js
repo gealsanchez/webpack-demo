35fad8819b7445ddbe8526d1996cf9a118eee833
gealsanchez <gealsanchez@gmail.com>
2022-11-09 15:55:12 -0500

    Webpack demo

--- webpack.config.js
+++ webpack.config.js
@@ -6,60 +6,66 @@ const json5 = require('json5');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-        }),
-    ],
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-        rules: [
-          {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-          },
-          {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: 'asset/resource',
-          },
-          {
-            test: /\.(woff|woff2|eot|ttf|otf)$/i,
-            type: 'asset/resource',
-          },
-          {
-            test: /\.(csv|tsv)$/i,
-            use: ['csv-loader'],
-          },
-          {
-            test: /\.xml$/i,
-            use: ['xml-loader'],
-          },
-          {
-            test: /\.toml$/i,
-            type: 'json',
-            parser: {
-              parse: toml.parse,
-            },
-          },
-          {
-            test: /\.yaml$/i,
-            type: 'json',
-            parser: {
-              parse: yaml.parse,
-            },
-          },
-          {
-            test: /\.json5$/i,
-            type: 'json',
-            parser: {
-              parse: json5.parse,
-            },
-          },
-        ],
+  mode: 'development',
+  entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
       },
-      
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ['csv-loader'],
+      },
+      {
+        test: /\.xml$/i,
+        use: ['xml-loader'],
+      },
+      {
+        test: /\.toml$/i,
+        type: 'json',
+        parser: {
+          parse: toml.parse,
+        },
+      },
+      {
+        test: /\.yaml$/i,
+        type: 'json',
+        parser: {
+          parse: yaml.parse,
+        },
+      },
+      {
+        test: /\.json5$/i,
+        type: 'json',
+        parser: {
+          parse: json5.parse,
+        },
+      },
+    ],
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
 };
